31b275fe6625a7cf13616be8de7b7f9650b2ee8d
Shamim Reza <wpshamim@gmail.com>
2022-05-30 15:36:24 +0600

    add review section

--- src/Components/Features/WhatWeDo/WhatWeDo.js
+++ src/Components/Features/WhatWeDo/WhatWeDo.js
@@ -10,8 +10,8 @@ const WhatWeDo = () => {
         { id: 4, title: "Social Media", description: "Preferred joy agreement put continual felicity elsewhere delivered now collect authority.", icon: faBox, iconColor: "#ea4c89" },
     ];
     return (
-        <div className="grid grid-cols-12 gap-10">
-            <div className="col-span-4">
+        <div className="grid grid-cols-12 gap-10 items-center py-10">
+            <div className="col-span-5 w-10/12">
                 <h4 className="mb-5 text-1xl font-semibold text-secondary">WHAT WE DO</h4>
                 <h2 className="mb-5 font-semibold text-4xl text-title">Let's Check our scientific Digital solutions</h2>
                 <p className="mb-5 text-paragraph">
@@ -20,10 +20,10 @@ const WhatWeDo = () => {
                 </p>
                 <button className="dude-btn">Discover More</button>
             </div>
-            <div className="col-span-8 grid grid-cols-2">
+            <div className="col-span-7 grid grid-cols-2">
                 {serviceItems.map((item) => (
-                    <div className="item">
-                        <FontAwesomeIcon className={`bg-[${item.iconColor}] text-white text-4xl p-5 rounded-full mb-5`} icon={item.icon} />
+                    <div className="item mb-10">
+                        <FontAwesomeIcon className={`bg-[${item.iconColor}] text-white text-4xl p-5 w-10 h-10 rounded-full mb-5`} icon={item.icon} />
                         <h4 className="text-2xl text-title font-semibold mb-5">{item.title}</h4>
                         <p className="text-paragraph">{item.description}</p>
                     </div>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Features from "../Features/Features";
+import Reviews from "../Reviews/Reviews";
 
 const Home = () => {
     return (
         <div>
             <Features></Features>
+            <Reviews></Reviews>
         </div>
     );
 };
